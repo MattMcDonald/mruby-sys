aaaf679b1a75ef80b613d9c27fb5f22c1f82960b
Matt McDonald <mail.matt.mcdonald@gmail.com>
2019-05-29 15:37:37 -0700

    trying to get bindgen to work

--- build.rs
+++ build.rs
@@ -1,3 +1,56 @@
+extern crate bindgen;
+extern crate cc;
+extern crate glob;
+
+use glob::glob;
+
+use std::env;
+use std::path::PathBuf;
+
+use std::process::Command;
+
+fn build() {
+    Command::new("cp")
+        .arg("-r")
+        .arg("vendor")
+        .arg("target")
+        .status()
+        .unwrap();
+    Command::new("make")
+        .current_dir("target/vendor")
+        .status()
+        .unwrap();
+
+    let mut build = cc::Build::new();
+    build.include("target/vendor/include");
+
+    for entry in glob("target/vendor/src/*.c").unwrap() {
+        if let Ok(path) = entry {
+            println!("{:?}", path.display());
+            build.file(path);
+        }
+    }
+
+    build.compile("mruby");
+    //     println!("cargo:rustc-link-search=native=target/vendor/build/host/lib");
+    //     println!("cargo:rustc-link-lib=static=mruby");
+    //     println!("cargo:rustc-link-lib=static=mruby_core");
+}
+
+fn bindgen() {
+    let bindings = bindgen::Builder::default()
+        .header("include/wrapper.h")
+        .whitelist_type("mrb_state")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
+
 fn main() {
-  
+    build();
+    bindgen();
 }
---
+++ include/wrapper.h
@@ -0,0 +1,1 @@
+#include "../vendor/include/mruby.h"
