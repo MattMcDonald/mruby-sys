fa63d694907030263be6e1d7754920b5091eea63
Matt McDonald <mail.matt.mcdonald@gmail.com>
2019-06-04 13:02:58 -0700

    fmt

--- build.rs
+++ build.rs
@@ -4,14 +4,16 @@ extern crate fs_extra;
 use std::env;
 use std::path::PathBuf;
 
-use std::process::Command;
 use fs_extra::dir::{copy, CopyOptions};
-
-
+use std::process::Command;
 
 fn out_path(p: &str) -> String {
-    let out_dir : PathBuf = PathBuf::from(env::var("OUT_DIR").expect("no OUT_DIR specified"));
-    let result = out_dir.join(p).to_str().expect("Couldn't convert path to string").to_string();
+    let out_dir: PathBuf = PathBuf::from(env::var("OUT_DIR").expect("no OUT_DIR specified"));
+    let result = out_dir
+        .join(p)
+        .to_str()
+        .expect("Couldn't convert path to string")
+        .to_string();
     println!("{}", result);
     result
 }
@@ -22,8 +24,7 @@ fn build() {
     let mut options = CopyOptions::new();
     options.copy_inside = true;
     options.overwrite = true;
-    copy("vendor", &target_vendor_path, &options)
-    .expect("Couldn't copy vendor to target");
+    copy("vendor", &target_vendor_path, &options).expect("Couldn't copy vendor to target");
 
     // we run make we that we can compile ruby libs to c
     Command::new("./minirake")
@@ -32,11 +33,13 @@ fn build() {
         .expect("Couldn't build mruby");
 
     println!("cargo:rustc-link-lib=static=mruby");
-    println!("cargo:rustc-link-search=native={}/build/host/lib", target_vendor_path);
+    println!(
+        "cargo:rustc-link-search=native={}/build/host/lib",
+        target_vendor_path
+    );
 }
 
 fn bindgen() {
-
     let bindings = bindgen::Builder::default()
         .header("include/wrapper.h")
         .opaque_type("mrb_heap_page") // tests were failing, this might be fixed in newer bindgen
